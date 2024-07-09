b5c1b0fe4921048c27694db07f53c3b10db93f19
shuaibohu <hushuaibo.hsb@alibaba-inc.com>
2024-07-09 17:16:39 +0800

    Update Shader Module

--- Pluto/ShaderFiles/Preprocess.cpp
+++ Pluto/ShaderFiles/Preprocess.cpp
@@ -2,26 +2,57 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+
+std::string GetContent(const std::string &file, const std::string &dir);
+nlohmann::json GetSpirv(std::string content);
+
 int main(int argc, char *argv[])
 {
     const char *shader_directory = argv[1];
     const char *output_directory = argv[2];
+    const std::string mapping[4] = {"Vertex", "Fragment", "Geometry", "Compute"};
     LoadShaderFiles::Init();
     std::cout << "Preprocess shader files in: " << shader_directory << std::endl;
+    std::cout << "Output shader files in: " << output_directory << std::endl;
     // Todo: convert and collect glsl to generate .shader.json files
     for (auto &shader : ShaderFilesMap)
     {
-        // auto vert_file = shader_directory + std::get<1>(shader.second);
-        // auto frag_file = shader_directory + std::get<1>(shader.second);
-        // std::ifstream ifs1(vert_file), ifs2(frag_file);
-        // std::ostringstream oss1, oss2;
-        // oss1 << ifs1.rdbuf();
-        // oss2 << ifs2.rdbuf();
-        // std::string vertContent = oss1.str();
-        // std::string fragContent = oss2.str();
-        // std::cout << vertContent << std::endl;
-        // std::cout << fragContent << std::endl;
-        // next step: read glsl and generate .shader.json files
+        nlohmann::json j;
+        std::vector<std::string> glsl_contents;
+        std::vector<std::string> shader_names;
+        shader_names.push_back(std::get<0>(shader.second));
+        shader_names.push_back(std::get<1>(shader.second));
+        shader_names.push_back(std::get<2>(shader.second));
+        shader_names.push_back(std::get<3>(shader.second));
+        for (int i = 0; i < 4; i++)
+        {
+            auto content = GetContent(shader_names[i], shader_directory);
+            if (content.empty())
+                continue;
+            j[mapping[i]]["type"] = mapping[i];
+            j[mapping[i]]["name"] = shader_names[i];
+            j[mapping[i]]["glsl"] = content;
+            j[mapping[i]]["spirv"] = GetSpirv(content); // todo
+        }
+        std::ofstream o(output_directory + shader.first + ".shader.json");
+        o << j << std::endl;
     }
+
     return 0;
+}
+std::string GetContent(const std::string &file, const std::string &dir)
+{
+    if (file.empty())
+        return "";
+    auto filepath = dir + file;
+    std::ifstream ifs(filepath);
+    std::ostringstream oss;
+    oss << ifs.rdbuf();
+    return oss.str();
+}
+
+nlohmann::json GetSpirv(std::string content)
+{
+    // todo
+    return nlohmann::json();
 }
