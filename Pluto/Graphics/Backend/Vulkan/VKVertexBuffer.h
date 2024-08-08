73f9a95c2ac863d9ae90159d960fc7d965dce60d
huhu <54528876+shuaibo919@users.noreply.github.com>
2024-08-08 07:36:14 +0000

    Update VKVertexBuffer

--- Pluto/Graphics/Backend/Vulkan/VKVertexBuffer.h
+++ Pluto/Graphics/Backend/Vulkan/VKVertexBuffer.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "Graphics/RHI/VertexBuffer.h"
+#include "Graphics/Backend/Vulkan/Vk.h"
 
 namespace pluto
 {
     namespace Graphics
     {
+        class VKBuffer;
         class VKVertexBuffer final : public VertexBuffer
         {
             friend class VKContext;
@@ -16,6 +18,12 @@ namespace pluto
         public:
             void Bind(const SharedPtr<CommandBuffer> &commandBuffer, const SharedPtr<Pipeline> &pipeline, uint8_t binding = 0) override;
             void Unbind() override;
+
+        public:
+            VKBuffer *&GetBuffer() { return mBuffer; }
+
+        private:
+            VKBuffer *mBuffer{nullptr};
         };
     }
 }
