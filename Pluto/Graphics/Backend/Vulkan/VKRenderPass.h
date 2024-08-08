e998ea642d5807793f6a91c54fb3680aad25347c
huhu <54528876+shuaibo919@users.noreply.github.com>
2024-08-08 08:22:08 +0000

    Add Methods in VKRenderPass

--- Pluto/Graphics/Backend/Vulkan/VKRenderPass.h
+++ Pluto/Graphics/Backend/Vulkan/VKRenderPass.h
@@ -1,7 +1,7 @@
 #pragma once
-
 #include "Graphics/RHI/RenderPass.h"
 
+#include "Graphics/Backend/Vulkan/Vk.h"
 namespace pluto
 {
     namespace Graphics
@@ -17,6 +17,10 @@ namespace pluto
         public:
             void BeginRenderPass(const SharedPtr<CommandBuffer> &commandBuffer, float (&clearColor)[4], const SharedPtr<Framebuffer> &frame, SubPassContents contents) const override;
             void EndRenderPass(const SharedPtr<CommandBuffer> &commandBuffer) override;
+
+        public:
+            /* TODO */
+            VkRenderPass GetHandle() const { return VK_NULL_HANDLE; }
         };
 
     }
