14dc2bffb557896a32cca6aa1290d3bc7dbff326
hsb <hsb919@126.com>
2024-08-12 22:04:20 +0800

    Update Preprocess

--- Pluto/ShaderFiles/Preprocess.cpp
+++ Pluto/ShaderFiles/Preprocess.cpp
@@ -210,7 +210,7 @@ pluto::Graphics::ShaderDataType SPIRVTypeToDataType(const spirv_cross::SPIRType 
 
 nlohmann::json ReflectFromSpirv(std::vector<uint32_t> spv, std::string type, std::string &toGLSL)
 {
-    uint32_t max_set = 0 = 0;
+    uint32_t max_set = 0;
     nlohmann::json j;
     spirv_cross::CompilerGLSL *glsl = new spirv_cross::CompilerGLSL(std::move(spv));
 
