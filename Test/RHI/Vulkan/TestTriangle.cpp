783a1f758499b1820ee2913568d868cb2b2dd4e4
huhu <54528876+shuaibo919@users.noreply.github.com>
2024-08-14 08:42:43 +0000

    Update Test

--- Test/RHI/Vulkan/TestTriangle.cpp
+++ Test/RHI/Vulkan/TestTriangle.cpp
@@ -28,7 +28,7 @@ int main()
     using namespace pluto::Graphics;
     RenderDevice::Init();
     auto ctx = GraphicsContext::Create(RenderAPI::VULKAN);
-    auto window = Window::Create(ctx, 600, 600, "Test");
+    auto window = Window::Create(ctx, 300, 100, "Test");
     ctx->Init();
     ctx->BindToDevice();
     ctx->SetMainSwapChain(window->GetSwapChain());
@@ -44,12 +44,13 @@ int main()
                              .Create(ctx);
 
     Texture2D::Ptr colorTarget = Texture::Builder()
-                                     .SetBase(600, 600, 1, RHIFormat::R16G16B16A16Float)
+                                     .SetBase(300, 100, 1, RHIFormat::R16G16B16A16Float)
                                      .SetFilter(TextureFilter::Linear, TextureFilter::Linear)
                                      .SetWrap(TextureWrap::ClampToedge)
                                      .Create(Texture::Type::Texture2D, ctx);
 
     CommandBuffer::Ptr cmdBuffer = CommandBuffer::Builder()
+                                       .SetUsageType(CommandBufferUsageType::RecycleSubmit)
                                        .Create(ctx);
 
     Pipeline::Ptr pipeline = Pipeline::Builder()
@@ -61,6 +62,29 @@ int main()
                                  .SetColorTarget(std::move(colorTarget), AttachmentType::Color)
                                  .SetClearTargets(true)
                                  .Create(ctx);
+    auto descriptorSet = DescriptorSet::Builder()
+                             .SetBindingLayout(shader, 0)
+                             .Create(ctx);
+
+    UniformDataMat4 model;
+    model.name = "model";
+    model.blockname = "UniformBufferObject";
+    model.data = glm::mat4(1.0f);
+
+    UniformDataMat4 view;
+    view.name = "view";
+    view.blockname = "UniformBufferObject";
+    view.data = glm::mat4(1.0f);
+
+    UniformDataMat4 projection;
+    projection.name = "proj";
+    projection.blockname = "UniformBufferObject";
+    projection.data = glm::mat4(1.0f);
+
+    descriptorSet->SetUniform(projection.blockname, projection.name, &projection.data);
+    descriptorSet->SetUniform(model.blockname, model.name, &model.data);
+    descriptorSet->SetUniform(view.blockname, view.name, &view.data);
+    descriptorSet->Update();
 
     while (!window->ShouldClose())
     {
