b504edf8489f3d081c43e5a97e6896ac46f623ca
hhae <hsb919@126.com>
2024-10-25 10:47:42 +0800

    Update Sample

--- Test/RHI/02_Texture.cpp
+++ Test/RHI/02_Texture.cpp
@@ -67,7 +67,7 @@ int main()
 float vertices[] = {
     -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
     0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-    0.0f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 0.5f, 1.0f,};
+    0.0f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 0.5f, 1.0f};
 
 struct UniformDataMat4
 {
