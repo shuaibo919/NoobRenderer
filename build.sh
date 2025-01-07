da589c3970df9f0a085c2697ee1d33f19614f752
shuaibo <hsb919@126.com>
2025-01-07 16:11:43 +0800

    add a parameter for  build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 # check parameters
 if [ $# -eq 0 ]; then
-    echo "[Error]: No backend specified!"
-    echo "[Help Message / Parameters]: sh $0 [backend](opengl/vulkan)"
-    echo "[Help Message / Example]:    sh $0  vulkan"
+    echo "[build.sh][Error]: No backend specified!"
+    echo "[build.sh][Help Message / Parameters]: sh $0 [backend](opengl/vulkan)"
+    echo "[build.sh][Help Message / Example]:    sh $0  vulkan"
     exit 1
 fi
 
@@ -19,10 +19,25 @@ case "$1" in
         BUILD_DIR="build-gl"
         ;;
     *)
-        echo "Unsupported backend '$1'. Supported backends are 'vulkan' and 'opengl'."
+        echo "[build.sh]:Unsupported backend '$1'. Supported backends are 'vulkan' and 'opengl'."
         exit 1
         ;;
 esac
 
+# check for -j parameter 
+# example : sh build.sh vulkan -p 64
+PARALLELISM=32
+for arg in "$@"; do
+    if [ "$arg" == "-p" ]; then
+        shift
+        PARALLELISM=$2
+        shift
+        echo "[build.sh]:CMake using $PARALLELISM parallel jobs to build."
+        break
+    fi
+    echo "[build.sh]:Default $PARALLELISM parallel jobs equal to 32, using -p [number] to change it."
+done
+
+
 cmake -S . -B "$BUILD_DIR" -DBUILD_WITH_VULKAN_BACKEND="$VULKAN_BACKEND"
-cmake --build "$BUILD_DIR" -j32
+cmake --build "$BUILD_DIR" -j"$PARALLELISM"
